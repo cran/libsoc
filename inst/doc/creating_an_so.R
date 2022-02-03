3205f975bf27331204e30734abb179cacd45100d
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2022-02-03 14:40:02 +0000

    version 0.7.3

--- inst/doc/creating_an_so.R
+++ inst/doc/creating_an_so.R
@@ -1,17 +1,17 @@
-## ----create_mle----------------------------------------------------------
+## ----create_mle---------------------------------------------------------------
 MLE <- data.frame(POP_CL=2.23, POP_V=40, IVCL=0.4, IVV=0.3)
 
-## ----add_columntypes-----------------------------------------------------
+## ----add_columntypes----------------------------------------------------------
 attributes(MLE)$columnType <- list(c("structuralParameter"), c("structuralParameter"), c("varParameter", "variance"), c("varParameter", "variance"))
 
-## ----create_popest_object------------------------------------------------
+## ----create_popest_object-----------------------------------------------------
 library(libsoc)
 popest <- so_PopulationEstimates$new()
 
-## ----attach_MLE----------------------------------------------------------
+## ----attach_MLE---------------------------------------------------------------
 popest$MLE <- MLE
 
-## ----parent_objects------------------------------------------------------
+## ----parent_objects-----------------------------------------------------------
 est <- so_Estimation$new()
 est$PopulationEstimates <- popest
 block <- so_SOBlock$new()
@@ -20,12 +20,12 @@ block$Estimation <- est
 so <- so_SO$new()
 so$add_SOBlock(block)
 
-## ----add_message---------------------------------------------------------
+## ----add_message--------------------------------------------------------------
 block$add_message("WARNING", "myTool", "name_of_warning", "This is the description", 2)
 
-## ----add_rawres----------------------------------------------------------
+## ----add_rawres---------------------------------------------------------------
 block$add_rawresults_datafile("Description", "file", "id1")
 
-## ----result--------------------------------------------------------------
+## ----result-------------------------------------------------------------------
 so
 
--- inst/doc/so.R
+++ inst/doc/so.R
@@ -1,63 +1,63 @@
-## ----read_so-------------------------------------------------------------
+## ----read_so------------------------------------------------------------------
 filename <- system.file("extdata", "pheno.SO.xml",  package="libsoc")
 so <- libsoc::so_SO_read(filename)
 
-## ----estimates-----------------------------------------------------------
+## ----estimates----------------------------------------------------------------
 param <- so$SOBlock[[1]]$Estimation$PopulationEstimates$MLE
 param
 
-## ----parameter_estimates-------------------------------------------------
+## ----parameter_estimates------------------------------------------------------
 so$all_population_estimates()
 
-## ----parameter_types-----------------------------------------------------
+## ----parameter_types----------------------------------------------------------
 cols <- colnames(param)
 so$variability_type(cols)
 
-## ----correlations--------------------------------------------------------
+## ----correlations-------------------------------------------------------------
 so$correlation_parameters(cols)
 
-## ----random_variables----------------------------------------------------
+## ----random_variables---------------------------------------------------------
 so$random_variable_from_variability_parameter(cols)
 
-## ----covariance_matrix---------------------------------------------------
+## ----covariance_matrix--------------------------------------------------------
 so$SOBlock[[1]]$Estimation$PrecisionPopulationEstimates$MLE$CovarianceMatrix
 
-## ----standard_error------------------------------------------------------
+## ----standard_error-----------------------------------------------------------
 so$SOBlock[[1]]$Estimation$PrecisionPopulationEstimates$MLE$StandardError
 
-## ----standard_error2-----------------------------------------------------
+## ----standard_error2----------------------------------------------------------
 so$all_standard_errors()
 
-## ----relative_standard_error---------------------------------------------
+## ----relative_standard_error--------------------------------------------------
 so$SOBlock[[1]]$Estimation$PrecisionPopulationEstimates$MLE$RelativeStandardError
 
-## ----OFV-----------------------------------------------------------------
+## ----OFV----------------------------------------------------------------------
 so$SOBlock[[1]]$Estimation$OFMeasures$Deviance
 
-## ----predictions---------------------------------------------------------
+## ----predictions--------------------------------------------------------------
 pred <- so$SOBlock[[1]]$Estimation$Predictions
 head(pred, 20)
 
-## ----columnType----------------------------------------------------------
+## ----columnType---------------------------------------------------------------
 attributes(pred)$columnType
 
-## ----id_column-----------------------------------------------------------
+## ----id_column----------------------------------------------------------------
 libsoc::id_column(pred)
 
-## ----id_column_name------------------------------------------------------
+## ----id_column_name-----------------------------------------------------------
 libsoc::id_column_name(pred)
 
-## ----idv_column----------------------------------------------------------
+## ----idv_column---------------------------------------------------------------
 libsoc::idv_column(pred)
 
-## ----idv_column_name-----------------------------------------------------
+## ----idv_column_name----------------------------------------------------------
 libsoc::idv_column_name(pred)
 
-## ----residuals-----------------------------------------------------------
+## ----residuals----------------------------------------------------------------
 res <- so$SOBlock[[1]]$Estimation$Residuals$ResidualTable
 head(res, 20)
 
-## ----messages------------------------------------------------------------
+## ----messages-----------------------------------------------------------------
 messages <- so$SOBlock[[1]]$TaskInformation$Message
 messages[[1]]$Name
 messages[[1]]$Content
@@ -65,7 +65,7 @@ messages[[1]]$Severity
 messages[[1]]$Toolname
 messages[[13]]$Content
 
-## ----iofv----------------------------------------------------------------
+## ----iofv---------------------------------------------------------------------
 iofv <- so$SOBlock[[1]]$Estimation$OFMeasures$IndividualContribToLL
 head(iofv)
 
